e29a9fd26ac3acc2a2f044256305313f01b42ef6
Nima Prmdi <nimaprmdi@gmail.com>
2022-11-27 18:33:31 +0330

    Fix Sass-mq issues

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -2,7 +2,7 @@ import Grid from "@mui/material/Grid";
 import { Box, Container } from "@mui/material";
 
 const Home = (): JSX.Element => {
-    return <section className="c-home">Home</section>;
+    return <section className="c-home">Haji</section>;
 };
 
 export default Home;
